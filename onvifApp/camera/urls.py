0a2d11420ce78de49e3bb93d2b91995ad5f10b6c
dmizheg <unlocker@tut.by>
2023-02-24 12:49:14 +0300

    ok

--- onvifApp/camera/urls.py
+++ onvifApp/camera/urls.py
@@ -1,17 +1,20 @@
-from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 from .views import(CameraView, CameraLoginView)
+from .views import osd_changeName
 from .views import my_view
+from .views import my_ajax_view
 
 
 urlpatterns = [
 
-		url(r'^(?P<id>[^\.]+)/$', CameraView.as_view(), 
+		re_path(r'^(?P<id>[^\.]+)/$', CameraView.as_view(), 
 			name = "camera_detail"),
-		url(r'^$', CameraLoginView.as_view(), 
+		re_path(r'^$', CameraLoginView.as_view(), 
 			name = "camera_login"),
-        path('my_view/', my_view, 
-            name='my_view'),
+	    re_path(r'^(?P<id>\d+)$', osd_changeName, name='osd_changeName'),
+        path('<int:param>', my_view, name='my_url'),
+        re_path(r'^(?P<id>\d+)$', my_ajax_view, name='my_ajax_view'),
+
 
             
 ]
